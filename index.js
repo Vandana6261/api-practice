cad130e3689c37052c3a7bf6a29ba3de84f50884
Vandana6261 <vvandanapatidar992@gmail.com>
2025-11-14 08:58:03 +0530

    404 error handle

--- index.js
+++ index.js
@@ -1,45 +1,54 @@
-let input = document.getElementById("inpText")
-let search = document.getElementById("btn")
+let input = document.getElementById("inpText");
+let search = document.getElementById("btn");
 
-let weatherCity = document.querySelector("#weather-city")
-let temp = document.querySelector("#temp")
-let humidity = document.querySelector("#humidity")
-let condition = document.querySelector("#condition")
-let windSpeed = document.querySelector("#wind-speed")
-// console.log(temp)
+let loadingMsg = document.querySelector("#loadingMsg");
+let weatherCity = document.querySelector("#weather-city");
+let temp = document.querySelector("#temp");
+let humidity = document.querySelector("#humidity");
+let condition = document.querySelector("#condition");
+let windSpeed = document.querySelector("#wind-speed");
 
+search.addEventListener("click", function () {
+  let city = input.value.toLowerCase();
+  let key = "aee1045382f4e2a05a39735d89481e80";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+  let loading = true;
+  if (loading) {
+    loadingMsg.textContent = "Loading";
+    weatherCity.textContent = "";
+    temp.textContent = "";
+    humidity.textContent = "";
+    windSpeed.textContent = "";
+    condition.textContent = "";
+  }
 
-search.addEventListener("click", function() {
-    let city = input.value.toLowerCase();
-    console.log(city)
-    let key = "aee1045382f4e2a05a39735d89481e80"
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`
-
-    getWeatherData(url).then((data) => {
-        console.log(data)
+  getWeatherData(url)
+    .then((data) => {
+      loading = false;
+      if (!loading) loadingMsg.textContent = "";
+      if (data.cod == 404) {
+        alert("Please enter a valid city name ");
+      } else {
+        console.log(data);
         weatherCity.textContent = `Weather in ${city}`;
-        temp.textContent = `Temperature: ${data.main.temp}`
-        humidity.textContent = `Humidity: ${data.main.humidity  }`;
+        temp.textContent = `Temperature: ${data.main.temp}`;
+        humidity.textContent = `Humidity: ${data.main.humidity}`;
         windSpeed.textContent = `Condition: ${data.wind.speed}`;
-        let cloud = data.clouds.all
-        if(cloud === 0) {
-            condition.textContent = "clear sky"
+        let cloud = data.clouds.all;
+        if (cloud === 0) {
+          condition.textContent = "clear sky";
         }
+      }
 
-        console.log(data.main)
     })
-
-})
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 function getWeatherData(url) {
-    return fetch(url)
-        .then((response) => response.json())
-        .then((data) => data)
-        .catch((error) => console.log(error))
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((error) => console.log(error));
 }
-
-
-
-
-
-
